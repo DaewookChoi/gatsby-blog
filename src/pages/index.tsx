5761a8fdf5f186effebb67ad6978ff6ed2aa7277
DaeWook Choi <dwchoi@ndream.com>
2022-10-07 09:44:25 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/common/GlobalStyle'
+import Introduction from 'components/main/Introduction'
+import Footer from 'components/common/Footer'
+
+const IndexPage: FunctionComponent = function () {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <Footer />
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+
+export default IndexPage
